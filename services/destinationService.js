15ec06b979e0d64b212fab5d5a0863dd2827b7c9
SyraxTarg <maimiti.saint-marc@efrei.net>
2024-03-26 16:37:34 +0100

    services

--- services/destinationService.js
+++ services/destinationService.js
@@ -1,1 +1,35 @@
-const {destination} = require('../models/destinationModel');
+const {Destination} = require('../models/destinationModel');
+
+
+//CRUD
+async function createDestination(destination) {
+    return await Destination.create(destination);
+}
+
+
+async function getDestinationById(destinationId){
+    const destination = await Destination.findByPk(destinationId);
+    return destination.toJSON();
+}
+
+async function getAllDestinations(criterias = {}){
+    const where = {};
+    if(criterias.nom){
+        where.nom = criterias.nom;
+    }
+    if(criterias.id){
+        where.id = criterias.id;
+    }
+    if(criterias.company){
+        where.company = criterias.company;
+    }
+    return await Destination.findAll({where});
+}
+
+async function deleteDestination(destinationId){
+    const destination = await Destination.findByPk(destinationId);
+    await destination.destroy();
+    return {datas: {supprime: "destination deleted"}};
+}
+
+module.exports = {  createDestination, getDestinationById, getAllDestinations, deleteDestination };
