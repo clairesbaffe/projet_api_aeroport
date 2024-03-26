0938fc5a4851f8ae0e21fb30187b764694c668f3
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-03-26 16:55:35 +0100

    controller vols

--- controllers/volController.js
+++ controllers/volController.js
@@ -1,1 +1,44 @@
-const volService = require('../services/volService');
+const volService = require("../services/volService");
+
+async function getVolById(req, res) {
+  try {
+    const id = req.params.id;
+    const vol = await volService.getVolById(id);
+
+    if (vol) {
+      res.json(vol);
+    } else {
+      res.json({ error: `Vol not found (id: ${id})` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+async function getAllVols(req, res) {
+  try {
+    const { destinationDepart, destinationArrivee, depart, arrivee, compagny } =
+      req.query;
+    const vols = await volService.getAllVols({
+      destinationDepart,
+      destinationArrivee,
+      depart,
+      arrivee,
+      compagny,
+    });
+    res.json(vols);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+async function createVol(req, res) {
+  try {
+    const vol = await volService.createVol(req.body);
+    res.json(vol);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+module.exports = { getVolById, getAllVols, createVol };
