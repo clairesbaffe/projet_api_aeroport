0d1223d781d807c0ec388cf3308c988e3577435c
SyraxTarg <maimiti.saint-marc@efrei.net>
2024-03-26 16:48:35 +0100

    controllers

--- controllers/destinationController.js
+++ controllers/destinationController.js
@@ -1,1 +1,45 @@
 const destinationService = require('../services/destinationService');
+
+async function getDestinationById(req, res) {
+    try {
+        const id = req.params.id;
+        const destination = await destinationService.getDestinationById(id);
+        if (destination) {
+          res.json(destination);
+        } else {
+          res.json({ "error": `Destination not found (id: ${id})` })
+        }
+      } catch (err) {
+        res.status(500).json({message: err.message})
+      }
+}
+
+async function getAllDestinations(req, res){
+    try{
+        const { nom, id, company } = req.query;
+        const destinations = await destinationService.getAllDestinations({nom, id, company});
+        res.json(destinations);
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+}
+
+async function createDestination(req, res){
+    try {
+        const destination = await destinationService.createDestination(req.body);
+        res.json(destination);
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
+}
+
+async function deleteDestination(req, res){
+    try{
+        const deletedDestination = await destinationService.deleteDestination(req.body);
+        res.json(deletedDestination);
+    } catch (err){
+        res.status(500).json({ message: err.message });
+    }
+}
+
+module.exports = { getDestinationById, getAllDestinations, createDestination, deleteDestination };
