fbe4fdae17e24eba0fc8624ecfd02dff5523976b
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-03-26 16:39:31 +0100

    service vols

--- services/volService.js
+++ services/volService.js
@@ -1,1 +1,31 @@
-const {vol} = require('../models/volModel');
+const { Vol } = require("../models/volModel");
+
+async function createVol(vol) {
+  return await Vol.create(hero);
+}
+
+async function getVolById(id) {
+  return await Vol.findByPk(id);
+}
+
+async function getAllVols(criterias = {}){
+    const where = {};
+    if(criterias.destinationDepart){
+        where.destinationDepart = criterias.destinationDepart;
+    }
+    if(criterias.destinationArrivee){
+        where.destinationArrivee = criterias.destinationArrivee;
+    }
+    if(criterias.depart){
+        where.depart = criterias.depart;
+    }
+    if(criterias.arrivee){
+        where.arrivee = criterias.arrivee;
+    }
+    if(criterias.compagny){
+        where.compagny = criterias.compagny;
+    }
+    return await Vol.findAll({ where });
+}
+
+module.exports = { createVol, getVolById, getAllVols };
